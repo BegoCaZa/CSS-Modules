e6ba3fe14629bbca333d7e75e3a01ad7e6edff06
BegoCaZa <begoxcz@hotmail.com>
2025-05-06 11:59:20 +0200

    sos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import Button from './components/button/Button';
 const App = () => {
   return (
     <>
-      <h1>React</h1> <Button />
+      <CardsContainer>
+        <Card
+          src=''
+          title='Sedans'
+          text='Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
+        />
+      </CardsContainer>
     </>
   );
 };
